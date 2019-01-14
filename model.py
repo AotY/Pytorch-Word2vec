5b2a47a60d5f6385e6e79b7f6ec35fccdfe716bd
AotY <694731929@qq.com>
2019-01-14 15:58:19 +0800

    first commit

--- model.py
+++ model.py
@@ -1,12 +1,8 @@
 # -*- coding: utf-8 -*-
 
-import numpy as np
-import torch as t
+import torch
 import torch.nn as nn
-
-from torch import LongTensor as LT
-from torch import FloatTensor as FT
-
+import numpy as np
 
 class Bundler(nn.Module):
 
@@ -26,10 +22,14 @@ class Word2Vec(Bundler):
         super(Word2Vec, self).__init__()
         self.vocab_size = vocab_size
         self.embedding_size = embedding_size
+
         self.ivectors = nn.Embedding(self.vocab_size, self.embedding_size, padding_idx=padding_idx)
         self.ovectors = nn.Embedding(self.vocab_size, self.embedding_size, padding_idx=padding_idx)
-        self.ivectors.weight = nn.Parameter(t.cat([t.zeros(1, self.embedding_size), FT(self.vocab_size - 1, self.embedding_size).uniform_(-0.5 / self.embedding_size, 0.5 / self.embedding_size)]))
-        self.ovectors.weight = nn.Parameter(t.cat([t.zeros(1, self.embedding_size), FT(self.vocab_size - 1, self.embedding_size).uniform_(-0.5 / self.embedding_size, 0.5 / self.embedding_size)]))
+
+        # init
+        self.ivectors.weight = nn.Parameter(torch.cat([torch.zeros(1, self.embedding_size), torch.FloatTensor(self.vocab_size - 1, self.embedding_size).uniform_(-0.5 / self.embedding_size, 0.5 / self.embedding_size)]))
+        self.ovectors.weight = nn.Parameter(torch.cat([torch.zeros(1, self.embedding_size), torch.FloatTensor(self.vocab_size - 1, self.embedding_size).uniform_(-0.5 / self.embedding_size, 0.5 / self.embedding_size)]))
+
         self.ivectors.weight.requires_grad = True
         self.ovectors.weight.requires_grad = True
 
@@ -37,12 +37,12 @@ class Word2Vec(Bundler):
         return self.forward_i(data)
 
     def forward_i(self, data):
-        v = LT(data)
+        v = torch.LongTensor(data)
         v = v.cuda() if self.ivectors.weight.is_cuda else v
         return self.ivectors(v)
 
     def forward_o(self, data):
-        v = LT(data)
+        v = torch.LongTensor(data)
         v = v.cuda() if self.ovectors.weight.is_cuda else v
         return self.ovectors(v)
 
@@ -53,23 +53,33 @@ class SGNS(nn.Module):
         super(SGNS, self).__init__()
         self.embedding = embedding
         self.vocab_size = vocab_size
+
         self.n_negs = n_negs
         self.weights = None
+
         if weights is not None:
             wf = np.power(weights, 0.75)
             wf = wf / wf.sum()
-            self.weights = FT(wf)
+            self.weights = torch.FloatTensor(wf)
 
     def forward(self, iword, owords):
         batch_size = iword.size()[0]
         context_size = owords.size()[1]
+
         if self.weights is not None:
-            nwords = t.multinomial(self.weights, batch_size * context_size * self.n_negs, replacement=True).view(batch_size, -1)
+            nwords = torch.multinomial(self.weights, batch_size * context_size * self.n_negs, replacement=True).view(batch_size, -1)
         else:
-            nwords = FT(batch_size, context_size * self.n_negs).uniform_(0, self.vocab_size - 1).long()
+            nwords = torch.FloatTensor(batch_size, context_size * self.n_negs).uniform_(0, self.vocab_size - 1).long()
+
         ivectors = self.embedding.forward_i(iword).unsqueeze(2)
         ovectors = self.embedding.forward_o(owords)
+
         nvectors = self.embedding.forward_o(nwords).neg()
-        oloss = t.bmm(ovectors, ivectors).squeeze().sigmoid().log().mean(1)
-        nloss = t.bmm(nvectors, ivectors).squeeze().sigmoid().log().view(-1, context_size, self.n_negs).sum(2).mean(1)
+
+        # 
+        oloss = torch.bmm(ovectors, ivectors).squeeze().sigmoid().log().mean(1)
+
+        nloss = torch.bmm(nvectors, ivectors).squeeze().sigmoid().log().view(-1, context_size, self.n_negs).sum(2).mean(1)
+
+        # loss
         return -(oloss + nloss).mean()
